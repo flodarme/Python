8b62e6c8f42dafab4596a6dc9c14323a97d79723
flodarme <44115527+flodarme@users.noreply.github.com>
2018-11-27 06:30:41 -0800

    Sample tutorial codes

---
+++ commentCode.py
@@ -0,0 +1,16 @@
+# This is my note
+
+"""This is my note
+and this is still my note
+and this is again is still my note.
+"""
+
+##this is a line of code. Alt 3
+
+
+def printMe():
+    """this is my docstring
+    """
+    print("Meeeeeeeeeeee!")
+
+print(printMe.__doc__)
