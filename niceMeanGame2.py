d9b0408f743d8c8b794847708702ca465209ce2b
Florence Darko-Mensah <florence.darkomensah@gmail.com>
2018-11-28 06:42:27 -0800

    Input and return function
    Tutorial for input and return

---
+++ niceMeanGame2.py
@@ -0,0 +1,22 @@
+
+
+
+def start():
+    print("Hello {}".format(get_name()))
+
+
+def get_name():
+    name = input("What is your name? ")
+    return name
+
+
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+    start()
